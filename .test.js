449b2f409b9676013a64030f34d21bdbbae62713
votantai3070 <votantai.07092002@gmail.com>
2023-06-15 13:59:01 +0700

    update check time

--- .test.js
+++ .test.js
@@ -2,24 +2,39 @@ function isValidTime(day, month, year) {
   // Perform your time validation logic here
   // For example, you can check if the day, month, and year are within valid ranges
 
-  // Validate the year
-  if (year < 0) {
-    return false; // Year cannot be negative
-  }
+  // Validate the day based on the month
+  // const daysInMonth = new Date(year, month, 0).getDate(); // Get the number of days in the month
+  if (day < 1 || day > daysInMonth(month, year)) return false; // Day should be between 1 and the number of days in the month
 
   // Validate the month
-  if (month < 1 || month > 12) {
-    return false; // Month should be between 1 and 12
-  }
+  if (month < 1 || month > 12) return false; // Month should be between 1 and 12
 
-  // Validate the day based on the month
-  const daysInMonth = new Date(year, month, 0).getDate(); // Get the number of days in the month
-  if (day < 1 || day > daysInMonth) {
-    return false; // Day should be between 1 and the number of days in the month
-  }
-
+  // Validate the year
+  if (isLeadYear(year)) return false;
+  // Year cannot be negative
   // Return true if the time is valid, otherwise false
   return true;
+}
+function isLeadYear(year) {
+  if (year % 4) {
+    if (year % 100) {
+      if (year % 400) {
+        return true;
+      }
+      return false;
+    }
+    return false;
+  }
+}
+
+function daysInMonth(month, year) {
+  if (month === 2) {
+    isLeadYear(year) ? 29 : 28;
+  } else if (month === 4 || month === 6 || month === 9 || month === 11) {
+    return 30;
+  } else {
+    return 31;
+  }
 }
 
 // Tests
@@ -31,16 +46,16 @@ test("Invalid time", () => {
   expect(isValidTime(22, 13, 2022)).toBe(false);
 });
 test("Invalid time", () => {
-  expect(isValidTime(29, 2, 2001)).toBe(false);
+  expect(isValidTime(29, 2, 2021)).toBe(false);
 });
 test("Valid time", () => {
-  expect(isValidTime(29, 2, 2000)).toBe(true);
+  expect(isValidTime(29, 2, 2020)).toBe(true);
 });
 test("Invalid time", () => {
-  expect(isValidTime(-1, 12, 2000)).toBe(false);
+  expect(isValidTime(-1, 12, 2022)).toBe(false);
 });
 test("Invalid time", () => {
-  expect(isValidTime(23, -1, 2000)).toBe(false);
+  expect(isValidTime(23, -1, 2022)).toBe(false);
 });
 test("Invalid time", () => {
   expect(isValidTime(23, 13, -2000)).toBe(false);
--- script.js
+++ script.js
@@ -1,47 +1,112 @@
-function checkTime() {
-  var day = parseInt(document.getElementById("dayInput").value);
-  var month = parseInt(document.getElementById("monthInput").value);
-  var year = parseInt(document.getElementById("yearInput").value);
+// function checkTime() {
+//   var day = parseInt(document.getElementById("dayInput").value);
+//   var month = parseInt(document.getElementById("monthInput").value);
+//   var year = parseInt(document.getElementById("yearInput").value);
 
-  var isValid = isValidTime(day, month, year);
+//   var isValid = isValidTime(day, month, year);
+
+//   var resultElement = document.getElementById("result");
+//   resultElement.innerHTML = isValid ? "Valid time" : "Invalid time";
+// }
+
+// function isValidTime(day, month, year) {
+//   // Check day, month, year have empty value
+//   if (isValidNumber(day) || isValidNumber(month) || isValidNumber(year)) {
+//     return false;
+//   }
+
+//   // Check day
+//   const dayInMonth = new Date(year, month, 0).getDate();
+//   if (day < 1 || day > dayInMonth) {
+//     return false;
+//   }
+
+//   // Check month
+//   if (isValidMonth(month)) {
+//     return false;
+//   }
+
+//   // Check year
+//   if (year < 0) {
+//     return false;
+//   }
+
+//   return true;
+// }
+
+// function isValidNumber(value) {
+//   return isNaN(value);
+// }
+
+// function isValidMonth(month) {
+//   return month < 1 || month > 12;
+// }
+
+// // // check day in month
+// // function getDaysInMonth(month, year) {
+// //   if (month === 2) {
+// //     return isLeapYear(year) ? 29 : 28;
+// //   } else if (month === 4 || month === 6 || month === 9 || month === 11) {
+// //     return 30;
+// //   } else {
+// //     return 31;
+// //   }
+// // }
+
+// // function isLeapYear(year) {
+// //   return (year % 4 && year % 100) || year % 400;
+// // }
+
+function checkTime() {
+  var day = document.getElementById("dayInput").value;
+  var month = document.getElementById("monthInput").value;
+  var year = document.getElementById("yearInput").value;
+
+  day = parseInt(day);
+  month = parseInt(month);
+  year = parseInt(year);
+
+  const isValid = isValidTime(day, month, year);
 
   var resultElement = document.getElementById("result");
-  resultElement.innerHTML = isValid ? "Valid time" : "Invalid time";
+  resultElement.innerHTML = isValid ? "Valid Time" : "Invalid Time";
 }
 
 function isValidTime(day, month, year) {
-  // Check day, month, year have empty value
-  if (isValidNumber(day) || isValidNumber(month) || isValidNumber(year)) {
-    return false;
-  }
+  //check empty input
+  if (isNaN(day) || isNaN(month) || isNaN(year)) return false;
 
-  // Check day
-  const dayInMonth = new Date(year, month, 0).getDate();
-  if (day < 1 || day > dayInMonth) {
-    return false;
-  }
+  //check day in month
+  // const daysInMonth = new Date(year, month, 0).getDate();
+  if (day < 1 || day > daysInMonth(month, year)) return false; //check day between 1 to days in this month
 
-  // Check month
-  if (isValidMonth(month)) {
-    return false;
-  }
+  // check month
+  if (month < 1 || month > 12) return false; //check month between 1 to 12
 
-  // Check year
-  if (isValueYear(year)) {
-    return false;
-  }
+  //check year
+  if (isLeadYear(year)) return false;
 
   return true;
 }
 
-function isValidNumber(value) {
-  return isNaN(value);
+function isLeadYear(year) {
+  if (year % 4) {
+    if (year % 100) {
+      if (year % 400) {
+        return true;
+      }
+      return false;
+    }
+    return false;
+  }
 }
 
-function isValidMonth(month) {
-  return month < 1 || month > 12;
-}
-
-function isValueYear(year) {
-  return year < 0;
+function daysInMonth(month, year) {
+  if (month === 2) {
+    isLeadYear(year) ? 29 : 28;
+  } else if (month === 4 || month === 6 || month === 9 || month === 11) {
+    return 30;
+  } else {
+    return 31;
+  }
 }
